b37516850f4d822facd611fcb043261b84123093
michelleto242 <72751940+michelleto242@users.noreply.github.com>
2022-11-23 22:34:15 -0800

    Update interactive.py

--- src/MT/interactive.py
+++ src/MT/interactive.py
@@ -93,7 +93,7 @@ def main(*args, src):
 
     # Initialize generator
     try: 
-        generator = task.build_generator(model, args)
+        generator = task.build_generator(models, args)
     except: 
         print(args) 
         print("ERROR") 
